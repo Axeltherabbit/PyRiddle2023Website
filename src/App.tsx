894d5d398ffe06c825bd69d4668b5693d64bfdcc
AxelTheRabbit <pietroconvalle@python.it>
2023-04-18 22:33:33 +0100

    finished arrows logic

--- src/App.tsx
+++ src/App.tsx
@@ -6,27 +6,79 @@ import { Square } from 'react-chessboard/dist/chessboard/types';
 import {pieces} from './Pieces';
 import {MovementsButtons} from './MovementsButtons';
 
+
+const boardSize = 200
+const files = "abcdefgh"
+const ranks = "12345678"
 function NumericToCoordinates(x: number, y: number){
   if (x > 7 || x < 0 || y > 7 || y < 0) return null;
-  let files = "abcdefgh"
-  return files[x]+String(y)
+  return files[x]+String(y+1)
 }
 
-function clearArrows(setArrows: Function) {
-  setArrows([]);
-  console.log("clearing arrows");
-}
-
-function drawArrows(setArrows: Function) {
-  setArrows([['a3', 'a5'], ['g1', 'f3']]);
-  console.log("drawing arrows");
+function CoordinatesToNumeric(coord: string){
+  if (coord.length != 2 || !files.includes(coord[0]) || !ranks.includes(coord[1])) return null;
+  return [files.indexOf(coord[0]), ranks.indexOf(coord[1])]
 }
 
 
+function CropArrowToBoard(x: number, y: number, dir : number[]){
+  dir = [dir[0]*-1, dir[1]*-1]
+  while (x < 0 || x > 7 || y < 0 || y > 7)
+  {
+    x += dir[0]
+    y += dir[1]
+  }
+  return [x, y]
+}
+
+
+const directions = [[[-1,1],[0,1],[1,1]],[[-1,0],null,[1,0]],[[-1,-1],[0,-1],[1,-1]]]
+function drawArrows(setArrows: Function, movements: (number | null)[][], currentPosition: string) {
+  let numericPosition = CoordinatesToNumeric(currentPosition); 
+  if (numericPosition === null) return;
+
+  let arrows : string[][] = []
+  for (let i=0; i<3; i++){
+    for (let j=0; j<3; j++){
+
+      let mov = movements[i][j]
+      let dir = directions[i][j];
+      if (mov !== null && dir !== null && mov > 0 ){ 
+        let vector = [dir[0]*mov, dir[1]*mov];
+        let arrowcoord = [numericPosition[0]+vector[0], numericPosition[1]+vector[1]];
+        arrowcoord = CropArrowToBoard(arrowcoord[0], arrowcoord[1], dir);
+        let arrow = NumericToCoordinates(arrowcoord[0], arrowcoord[1])
+        let arrowStartNumeric = [numericPosition[0]+dir[0], numericPosition[1]+dir[1]]
+        let arrowStart = NumericToCoordinates(arrowStartNumeric[0], arrowStartNumeric[1])
+        if (arrowStart === null) continue;
+        if (arrow !== null && arrow !== currentPosition) arrows.push([arrowStart, arrow]);
+      }
+
+    }
+  }
+  setArrows(arrows);
+}
+function OnPositionChange(currentPosition : Map<string, string>, setArrows: Function, 
+                          setPiecePosition: Function, movements : (number | null)[][] ){
+  for (var k in currentPosition) {
+
+    drawArrows(setArrows, movements, k); 
+    setPiecePosition(k);
+  }
+
+  return true;
+}
+function refreshArrows(setArrowsMovements : Function, movements: (number | null)[][], piecePosition1: string, setArrowsCaptures: Function, captures: (number | null)[][], piecePosition2: string){
+  drawArrows(setArrowsMovements, movements, piecePosition1);
+  drawArrows(setArrowsCaptures, captures, piecePosition2);
+}
 function App() {
-  const [arrows, setArrows] = useState<Square[][]>([]);
+  const [arrowsMovements, setArrowsMovements] = useState<Square[][]>([]);
+  const [arrowsCaptures, setArrowsCaptures] = useState<Square[][]>([]);
   const [movements, setMovements] = useState<(number | null)[][]>([[0,0,0],[0,null,0],[0,0,0]]);
   const [captures, setCaptures] = useState<(number | null)[][]>([[0,0,0],[0,null,0],[0,0,0]]);
+  const [piecePosition1, setPiecePosition1] = useState<string>("d5")
+  const [piecePosition2, setPiecePosition2] = useState<string>("d5")
 
   return (
     <div className="App container">
@@ -36,16 +88,30 @@ function App() {
       <div className='row'>
         <div className='col'>
           <h3 className="text-primary">Movements</h3>
-          <MovementsButtons movements={movements} setMovements={setMovements} captures={captures} setCaptures={setCaptures}/>
+          <MovementsButtons movements={movements} setMovements={setMovements} 
+            captures={captures} setCaptures={setCaptures} 
+            refreshArrows={()=> refreshArrows(setArrowsMovements, movements, piecePosition1,setArrowsCaptures, captures, piecePosition2)}/>
         </div>
 
         <div className='col'>
-          <Chessboard
-            position="8/8/8/3K4/8/8/8/8"
-            customPieces={pieces}
-            onPieceDragBegin={() => clearArrows(setArrows)}
-            onPieceDragEnd={() => drawArrows(setArrows)}
-            customArrows={arrows} />
+          <div className='col'>
+            <Chessboard
+              boardWidth={boardSize}
+              customPieces={pieces}
+              customArrows={arrowsMovements} 
+              position="8/8/8/3K4/8/8/8/8"
+              customArrowColor="green"
+              getPositionObject={(currentPosition) => OnPositionChange(currentPosition, setArrowsMovements, setPiecePosition1, movements)}   
+              />
+            <Chessboard
+              boardWidth={boardSize}
+              customPieces={pieces}
+              customArrows={arrowsCaptures} 
+              position="8/8/8/3K4/8/8/8/8"
+              customArrowColor="red"
+              getPositionObject={(currentPosition) => OnPositionChange(currentPosition, setArrowsCaptures, setPiecePosition2, captures)}   
+              />
+          </div>
           <p className='text-primary'>Drag and drop the piece around to see the squares coverage</p>
         </div>
       </div>
--- src/MovementsButtons.tsx
+++ src/MovementsButtons.tsx
@@ -4,17 +4,18 @@ import React from 'react';
 import {ArrowUpLeft, ArrowUp, ArrowUpRight, ArrowLeft, ArrowRight, ArrowDownLeft, ArrowDown, ArrowDownRight} from 'react-bootstrap-icons';
 import { Button, Form } from 'react-bootstrap';
 
-function updateMovement(current : (number | null)[][], setState: Function, x: number, y: number, toggle : boolean){
+function updateMovement(current : (number | null)[][], setState: Function, x: number, y: number, toggle : boolean, refreshArrows: Function){
   let increase = toggle ? 1 : -1 ;
   let new_state = [...current];
   if (new_state[x][y] !== null) new_state[x][y] = Math.min(Math.max(new_state[x][y] + increase, 0),7);
   setState(new_state);
+  refreshArrows();
 }
 
 
-type Props = {movements : (number | null)[][], setMovements : Function, captures : (number | null)[][], setCaptures : Function}
+type Props = {movements : (number | null)[][], setMovements : Function, captures : (number | null)[][], setCaptures : Function, refreshArrows: Function}
 
-export const MovementsButtons : React.FC<Props> = ({movements, setMovements, captures, setCaptures}) => {
+export const MovementsButtons : React.FC<Props> = ({movements, setMovements, captures, setCaptures, refreshArrows}) => {
   const [toggle, setToggle] = useState<boolean>(true);
     
   const Arrows = [[ArrowUpLeft, ArrowUp, ArrowUpRight], [ArrowLeft, null, ArrowRight], [ArrowDownLeft, ArrowDown, ArrowDownRight]];
@@ -22,11 +23,11 @@ export const MovementsButtons : React.FC<Props> = ({movements, setMovements, cap
     <div className='container'>
       {
       [...Array(3)].map((_, i) => (
-            <div className='col d-flex justify-content-between' >
+            <div key={`div${i}`} className='col d-flex justify-content-between' >
               {
                 [...Array(3)].map((_, j) => {
                   if (Arrows[i][j] === null) return ( // middle toggle switch
-                  <Form.Group className={toggle ? "text-success" : "text-danger"}>
+                  <Form.Group key="switch" className={toggle ? "text-success" : "text-danger"}>
                     <Form.Check
                       type="switch"
                       defaultChecked
@@ -35,9 +36,9 @@ export const MovementsButtons : React.FC<Props> = ({movements, setMovements, cap
                     />
                   </Form.Group>);
 
-                  return (<div>
-                    <Button className='btn btn-success' onClick={()=>updateMovement(movements, setMovements, i, j, toggle)}>{React.createElement(Arrows[i][j])}{movements[i][j]}</Button> 
-                    <Button className='btn btn-danger' onClick={()=>updateMovement(captures, setCaptures, i, j, toggle)}>{React.createElement(Arrows[i][j])}{captures[i][j]}</Button>
+                  return (<div key={`div${i}${j}`}>
+                    <Button key={`bs${i}${j}`} className='btn btn-success' onClick={()=>updateMovement(movements, setMovements, i, j, toggle, refreshArrows)}>{React.createElement(Arrows[i][j])}{movements[i][j]}</Button> 
+                    <Button key={`bd${i}${j}`} className='btn btn-danger' onClick={()=>updateMovement(captures, setCaptures, i, j, toggle, refreshArrows)}>{React.createElement(Arrows[i][j])}{captures[i][j]}</Button>
                     </div>);
                 })
               }
